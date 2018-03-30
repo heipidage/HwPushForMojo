9f4c64dd076e397dc18a6cdc34dfed1fcee0f107
heipidage <heipidage007@gmail.com>
2018-03-30 08:50:38 -0400

    update

--- autoget.sh
+++ autoget.sh
@@ -1,8 +1,9 @@
 # !/bin/bash
-echo "Begin" 
-perl get.pl > hw_access_token_gcm.txt
+echo "Begin"
+cd /root/HwPushForMojo/ 
+perl /root/HwPushForMojo/get.pl > /root/HwPushForMojo/hw_access_token_gcm.txt
 wait
-git add .
+git add /root/HwPushForMojo/
 git commit -m 'update'
 git push --force origin master 
 echo "Done"
